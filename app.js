05135c05202a503b53ee9e8f45d846fae81291b3
Wei9023 <vv880923@gmail.com>
2019-01-11 16:09:22 -0800

    Stretch goal complete

--- app.js
+++ app.js
@@ -139,13 +139,20 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-// var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+ var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-// function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
-// }
-
+ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var multLong = dynamicArray[0]; 
+    for(var i=0;i<dynamicArray.length;i++){
+        
+        multLong = multiply(multLong, dynamicArray[i])[0];
+        console.log(multLong);
+    }
+var message = 'The numbers ' + testDynamicArray[0] + ',' + testDynamicArray[1] + ',' + testDynamicArray[2] + ',' + testDynamicArray[3] + ',' + testDynamicArray[4] + ' have a product of ' + multLong + '.';
+return [multLong, message];
+}
+  
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+ testMultiplyAnyArray(testDynamicArray);
 
 /* Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas. */
